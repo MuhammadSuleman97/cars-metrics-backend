e7d83ca122c9519940d42d6c83d8290a4a275588
Muhammad Suleman <53903082+MuhammadSuleman97@users.noreply.github.com>
2024-09-04 16:08:29 +1000

    Node Project created

---
+++ index.js
@@ -0,0 +1,12 @@
+// node abckend
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
