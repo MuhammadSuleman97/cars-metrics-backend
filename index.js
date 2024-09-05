52f6080c8ed398a240857ed7b5f420b9982eb313
Muhammad Suleman <53903082+MuhammadSuleman97@users.noreply.github.com>
2024-09-05 14:51:21 +1000

    updated dist added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { clearDirectory } from './helpers/fileUtils.js';
 import router from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
@@ -17,27 +18,23 @@ const UPLOADS_DIR = path.join(__dirname, 'uploads');
 // Clear the uploads folder at startup to remove any files that were left over from previous runs
 clearDirectory(UPLOADS_DIR);
 
+// Use CORS middleware
+app.use(cors({
+  origin: '*', // Allow requests from any origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
+}));
+
 // Serve static files from the Angular browser directory
 app.use(express.static(path.join(__dirname, 'dist', 'cars-metrics', 'browser')));
 
 // API routes
 app.use('/api', router);
 
-
 // Redirect all other routes to Angular's index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'cars-metrics', 'browser', 'index.html'));
 });
-
-// Allow CORS requests
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-
 
 // Handle 404 errors
 app.use((req, res, next) => {
