9549fa4ef38f27009887a38eb4571eace37f97f9
Muhammad Suleman <53903082+MuhammadSuleman97@users.noreply.github.com>
2024-09-05 15:31:57 +1000

    Angular build with no Cars check added

--- dist/cars-metrics/server/main.server.mjs
+++ dist/cars-metrics/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3QYX3SN6.mjs";import"./chunk-QVQV2Y52.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-VHIQDO4A.mjs";import"./chunk-QVQV2Y52.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
